bf9c6d99f9a93822d52aae59eae25b5a78f3cbc6
SigurdNybergM <elev516156@elev.trondelagfylke.no>
2025-09-02 15:35:21 +0200

    til oppg 3

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+var myName = 'Sigurd';
+console.log('Hei ' + myName);
+
+let randomedNumber = Math.floor(Math.random()*10);
+let randomedNumber2 = Math.floor(Math.random()*10);
+console.log(+randomedNumber + randomedNumber2);
+console.log(randomedNumber2 - randomedNumber);
+console.log(randomedNumber2 * randomedNumber);
+console.log(randomedNumber2 / randomedNumber);
+
+var alder = Math.floor(Math.random()*1000);
+if (alder >= 18) {console.log('Du er myndig')}
+else {console.log('Du er ikke myndig')}
 
